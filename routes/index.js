9926e379c4d76d78ee2b81608c8b10ebac6c4723
ngmh <ngmhcartoon@gmail.com>
2021-04-24 03:45:38 +0800

    messing around with cookies, can store sentiment array for the pie chart in here

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var cookie = req.cookies.data;
+  if(cookie === undefined){
+    res.cookie("data", 0, {maxAge: 999999});
+  } else {
+    console.log(cookie);
+    counter = parseInt(cookie);
+    res.cookie("data", counter+1, {maxAge: 999999});
+  }
+
+  res.render('index', { title: cookie });
 });
 
 module.exports = router;
