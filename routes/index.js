a29472e2810814ff20e7ddf286474097d39e3948
ngmh <ngmhcartoon@gmail.com>
2021-04-24 04:27:01 +0800

    can make weird mood graph pie chart thingy

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var cookie = req.cookies.data;
-  if(cookie === undefined){
-    res.cookie("data", 0, {maxAge: 999999});
-  } else {
-    console.log(cookie);
-    counter = parseInt(cookie);
-    res.cookie("data", counter+1, {maxAge: 999999});
-  }
+  res.render('index', { title: "Unplug." });
+});
 
-  res.render('index', { title: cookie });
+function evaluate(text){
+  if(text.includes("sad")){
+    return "sad";
+  } else return "happy";
+}
+
+router.post('/', function(req, res, next) {
+  feeling = evaluate(req.body.feeling);
+  current = req.cookies.data;
+  if(current === undefined){
+    current = [];
+  }
+  current.push(feeling);
+  res.cookie("data", current);
+  res.render('index', { title: "Unplug.", feeling: feeling, current: current});
 });
 
 module.exports = router;
