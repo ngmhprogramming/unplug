ea6cea935863a3b1e659c62c81b073921d382e53
ngmh <ngmhcartoon@gmail.com>
2021-04-24 22:42:12 +0800

    api works woo

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,50 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Unplug." });
+  current = req.cookies.data;
+  if(current === undefined){
+    current = [];
+    res.cookie("data", current);
+  }
+  res.render('index', { title: "Unplug.", current: current});
 });
 
-function evaluate(text){
-  emotions = ["happy", "sad", "angry", "fear", "calm", "confusion"];
-  for(const emotion of emotions){
-    if(text.includes(emotion)) return emotion;
-  }
-  return "happy";
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+async function evaluate(text){
+  text += " ";
+  const toneAnalyzer = new ToneAnalyzerV3({
+    iam_apikey: "q6qFtVqLgf1so0kWuXkLsNTG7rrEKefShu9Ctx_OEvw0",
+    url: 'https://api.kr-seo.tone-analyzer.watson.cloud.ibm.com/instances/f071a1ab-4152-420c-a0fd-ad9d6b72db45',
+    version: '2017-09-21',
+  });
+
+  valid = ["anger", "fear", "joy", "sadness"];
+  emotion = "Unknown";
+  const result = await new Promise((resolve, reject) => {
+    toneAnalyzer.tone({tone_input:{text: text}}, (err, resp) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        let tones = resp.document_tone.tones;
+        console.log(tones);
+
+        var score = -1;
+        for (let v of tones) {
+          if(v.score > score && v.score > 0.1 && valid.includes(v.tone_id)){
+            score = v.score;
+            emotion = v.tone_name;
+          }
+        }
+        resolve(emotion);
+      }
+    });
+  });
+  return result;
 }
 
-router.post('/', function(req, res, next) {
-  feeling = evaluate(req.body.feeling);
+router.post('/', async function(req, res, next) {
+  feeling = await evaluate(req.body.feeling);
   current = req.cookies.data;
   if(current === undefined){
     current = [];
--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const toneAnalyzer = new ToneAnalyzerV3({
-    iam_apikey: "q6qFtVqLgf1so0kWuXkLsNTG7rrEKefShu9Ctx_OEvw0",
-    url: 'https://api.kr-seo.tone-analyzer.watson.cloud.ibm.com/instances/f071a1ab-4152-420c-a0fd-ad9d6b72db45',
-    version: '2017-09-21',
-  });
-  const text = 'i am tentative';
-
-  //anger fear joy sadness confident tentative
-  valid = ["anger", "fear", "joy", "sadness"];
-  toneAnalyzer.tone({tone_input:{text: text}}, (err, resp) => {
-
-    if (err) {
-      console.log(err);
-    } else {
-      let tones = resp.document_tone.tones;
-      console.log(tones);
-  
-      var score = -1, emotion = "Unknown";
-      let final = emotion;
-      for (let v of tones) {
-        if(v.score > score && valid.includes(v.tone_id)){
-          score = v.score;
-          emotion = v.tone_name;
-          console.log(emotion);
-        }
-      }
-    }
-    res.send("you are " + emotion);
-  });
+  res.send("go away");
 });
 
 module.exports = router;
